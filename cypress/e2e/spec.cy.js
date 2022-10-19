e03ba5dc1110af303b06315d7632fa8a80023eb1
Alexander Goriachnick <alexandergo@justice.gov.il>
2022-10-19 10:34:37 +0300

    sample code that fails on chrome but works in ff

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 describe('page', () => {
   it('works', () => {
-    cy.visit('https://example.cypress.io')
+    cy.visit('https://alexdebug-moj.cs88.force.com/s/mytest')
+    cy.get('.uiOutputRichText').contains('Use this HTML Editor to add your own markup.')
   })
 })
